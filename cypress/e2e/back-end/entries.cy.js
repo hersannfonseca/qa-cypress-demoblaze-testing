8c4f5f51e64c0579651e2e358a8c113f3abc9f20
Hersann FOnseca <164311396+hersannfonseca@users.noreply.github.com>
2024-03-29 13:32:28 -0600

    add ajv library

--- cypress/e2e/back-end/entries.cy.js
+++ cypress/e2e/back-end/entries.cy.js
@@ -1,3 +1,5 @@
+import { getAllEntriesSchema } from "../../schemas/get-all-entries-schema";
+
 /// <reference types="cypress" />
 
 describe('Entries feature', () => {
@@ -5,6 +7,7 @@ describe('Entries feature', () => {
         cy.request("GET", "https://api.demoblaze.com/entries").then((response) => {
             expect(response.status).to.eq(200);
             expect(response.body).to.not.be.null;
+            cy.validateSchema(getAllEntriesSchema, response.body);
             cy.log(JSON.stringify(response.body));
         });
     });
